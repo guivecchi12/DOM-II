102108c1ff3b01785ce33861e872b26f7ec054d9
unknown <guivecchi12@gmail.com>
2020-04-09 20:14:31 -0600

    start of project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let bus = document.querySelector('.intro img');
+let body = document.querySelector('body');
+bus.addEventListener("click", (e)=>{
+    e.target.style.width += "50%" ;
+    e.target.style.transform += "rotate(45deg)";  
+});
